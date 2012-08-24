bac1b2657b5ec310eb3ed05d2a38d119b33fe166
Jesse Bikman <jessebikman@gmail.com>
2012-08-24 01:13:31 -0400

    Tidied up code to (mostly) follow PEP8
    Everything checks out to follow PEP8 except for "E125 continuation line
    does not distinguish itself from next logical line" [used pep8 style
    guide checker - http://pypi.python.org/pypi/pep8/]

--- game.py
+++ game.py
@@ -11,6 +11,7 @@ init()
 # Initializes board
 board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
+
 # Prints x's in magenta and o's in green
 def colorprint(board, index):
     if board[index] == 'x':
@@ -19,41 +20,57 @@ def colorprint(board, index):
         return Fore.GREEN + board[index] + Fore.RESET
     else:
         return board[index]
-# Clears terminal window via os commands, checks operating system to make 
+
+
+# Clears terminal window via os commands, checks operating system to make
 # sure which command is kosher
 def cleanup():
-    os.system('cls' if os.name=='nt' else 'clear')
+
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 
 # Prints board
-def show(board):   
-    print colorprint(board, 0), Fore.CYAN + '|' + Fore.RESET, colorprint(board, 1), Fore.CYAN + '|' + Fore.RESET, colorprint(board, 2)
+def show(board):
+
+    print colorprint(board, 0), Fore.CYAN + '|' + Fore.RESET, \
+        colorprint(board, 1), Fore.CYAN + '|' + Fore.RESET, \
+        colorprint(board, 2)
     print Fore.CYAN + '-' * 9 + Fore.RESET
-    print colorprint(board, 3), Fore.CYAN + '|' + Fore.RESET, colorprint(board, 4), Fore.CYAN + '|' + Fore.RESET, colorprint(board, 5)
+    print colorprint(board, 3), Fore.CYAN + '|' + Fore.RESET, \
+        colorprint(board, 4), Fore.CYAN + '|' + Fore.RESET, \
+        colorprint(board, 5)
     print Fore.CYAN + '-' * 9 + Fore.RESET
-    print colorprint(board, 6), Fore.CYAN + '|' + Fore.RESET, colorprint(board, 7), Fore.CYAN + '|' + Fore.RESET, colorprint(board, 8)
+    print colorprint(board, 6), Fore.CYAN + '|' + Fore.RESET, \
+        colorprint(board, 7), Fore.CYAN + '|' + Fore.RESET, \
+        colorprint(board, 8)
+
 
 # Allows player to choose their next move
-def chooser(x_or_o,board):
+def chooser(x_or_o, board):
+
     while True:
         show(board)
         try:
             x_or_o_input = input("Select a spot[%s]> " % x_or_o)
             x_or_o_input = int(x_or_o_input)
-            if board[x_or_o_input-1] == 'x' or board[x_or_o_input-1] == 'o':
+            # Subtract 1 to accomodate for indexing vs regular
+            # human sensibility
+            x_or_o_input -= 1
+            if board[x_or_o_input] == 'x' or board[x_or_o_input] == 'o':
                 cleanup()
                 print "This spot has already been taken:"
                 continue
             else:
-                board[x_or_o_input-1] = x_or_o
+                board[x_or_o_input] = x_or_o
                 break
         except IndexError:
             continue
         except ValueError:
-           print("That is not a number on the board.")
-           continue
+            print("That is not a number on the board.")
+            continue
         except TypeError:
-           print("That is not a number on the board.")
-           continue
+            print("That is not a number on the board.")
+            continue
         except IndexError:
             print("That is not a number on the board.")
             continue
@@ -64,21 +81,32 @@ def chooser(x_or_o,board):
             print("That is not a number on the board.")
             continue
 
+
 # Checks to see if there's a winner
-def victory_evaluator(board,x_or_o):
-    if (board[0] == x_or_o and board[1] == x_or_o and board[2] == x_or_o) or (board[3] == x_or_o and board[4] == x_or_o and board[5] == x_or_o) or (board[6] == x_or_o and board[7] == x_or_o and board[8] == x_or_o):
+def victory_evaluator(board, x_or_o):
+
+    if board[0] == x_or_o and board[1] == x_or_o and board[2] == x_or_o or \
+        board[3] == x_or_o and board[4] == x_or_o and board[5] == x_or_o or \
+        board[6] == x_or_o and board[7] == x_or_o and board[8] == x_or_o:
         show(board)
         return True
-    elif (board[0] == x_or_o and board[3] == x_or_o and board[6] == x_or_o) or (board[1] == x_or_o and board[4] == x_or_o and board[7] == x_or_o) or (board[2] == x_or_o and board[5] == x_or_o and board[8] == x_or_o):
+    elif board[0] == x_or_o and board[3] == x_or_o and board[6] == x_or_o or \
+        board[1] == x_or_o and board[4] == x_or_o and board[7] == x_or_o or \
+        board[2] == x_or_o and board[5] == x_or_o and board[8] == x_or_o:
         show(board)
         return True
-    elif (board[0] == x_or_o and board[4] == x_or_o and board[8] == x_or_o) or (board[2] == x_or_o and board[4] == x_or_o and board[6] == x_or_o):
+    elif board[0] == x_or_o and board[4] == x_or_o and board[8] == x_or_o or \
+        board[2] == x_or_o and board[4] == x_or_o and board[6] == x_or_o:
         show(board)
         return True
 
+
 # Crudely verifies that a tie has taken place
 def tie_evaluator(board):
-    if(board[0] != 1 and board[1] != 2 and board[2] != 3 and board[3] != 4 and board[4] != 5 and board[5] != 6 and board[6] != 7 and board[7] != 8 and board[8] != 9):
+
+    if board[0] != 1 and board[1] != 2 and board[2] != 3 and \
+        board[3] != 4 and board[4] != 5 and board[5] != 6 and \
+        board[6] != 7 and board[7] != 8 and board[8] != 9:
         show(board)
         return True
 
@@ -88,7 +116,7 @@ gameover = False
 # Main game loop
 while True:
     cleanup()
-    chooser('x',board)
+    chooser('x', board)
     gameover = victory_evaluator(board, 'x')
     if gameover is True:
         print "You won, player who chose 'x'!"
@@ -98,7 +126,7 @@ while True:
         print "Cats game!"
         break
     cleanup()
-    chooser('o',board)
+    chooser('o', board)
     gameover = victory_evaluator(board, 'o')
     if gameover is True:
         print "You won, player who chose 'o'!"
